ca9a127212f73f93d8e393f4c533bf2bd110071a
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-04 20:24:31 +0300

    feat: add task context provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import Container from '@mui/material/Container';
 import Header from './components/Header';
 import ShowTasks from './components/task/ShowTasks';
 import AddTask from './components/task/AddTask';
+import { TaskContextProvider } from './store/taskcontext';
 
 function App() {
 
   return (
-    <>
+    <TaskContextProvider>
       <Header />
       <Container maxWidth="sm">
         <AddTask />
         <ShowTasks />
       </Container>
-    </>
+    </TaskContextProvider>
   )
 }
 
--- src/store/taskcontext.tsx
+++ src/store/taskcontext.tsx
@@ -8,4 +8,10 @@ const TaskContext = createContext({
     deleteTask : (id: string) => {},
 })
 
+export const TaskContextProvider = (props : unknown) => {
+    return <TaskContext.Provider>
+        {props.chiildren}
+    </TaskContext.Provider>
+}
+
 export default TaskContext
