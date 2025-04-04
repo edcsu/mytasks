988252a91ad968ced4719ae804eb8ad6c4cd75de
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-04 17:51:52 +0300

    style: initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
-
+import Container from '@mui/material/Container';
 function App() {
 
   return (
     <>
+    <Container maxWidth="sm">
+    </Container>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,6 +13,9 @@ import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
