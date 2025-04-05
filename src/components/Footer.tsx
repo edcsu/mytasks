a5eea48bb7314920ac618e777d0b1de091528927
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-05 17:08:20 +0300

    style: fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/X';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Copyright = () => {
   return (
@@ -31,6 +32,9 @@ const Footer = () => {
         gap: { xs: 4, sm: 8 },
         py: { xs: 8, sm: 10 },
         textAlign: { sm: 'center', md: 'left' },
+        position : 'fixed',
+        bottom: 0,
+        left: 0
       }}
     >
       <Box
@@ -78,6 +82,15 @@ const Footer = () => {
             sx={{ alignSelf: 'center' }}
           >
             <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            size="small"
+            href="mailto:skeith696@gmail.com"
+            aria-label="email"
+            sx={{ alignSelf: 'center' }}
+          >
+            <EmailIcon />
           </IconButton>
         </Stack>
       </Box>
